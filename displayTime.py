e2ff27e589241f2b7b174d8c1ec51d211275eed0
tesseradecades <nwe8978@rit.edu>
2017-04-26 12:05:15 -0400

    refactored the set methods

--- displayTime.py
+++ displayTime.py
@@ -19,56 +19,35 @@ def setHours(hh=0):
 
     #hh 1s place
     ones = '{0:04b}'.format(int(str(hh)[1]))
-    if(ones[3] == "1"):
-        sense.set_pixel(1,7,blue)
-    if(ones[2] == "1"):
-        sense.set_pixel(1,6,blue)
-    if(ones[1] == "1"):
-        sense.set_pixel(1,5,blue)
-    if(ones[0] == "1"):
-        sense.set_pixel(1,4,blue)
+    for i in range(4):
+        if(ones[i] == "1"):
+            sense.set_pixel(1,4+i,blue)
 
 def setMinutes(mm=0):
     #mm 10s place
     tens = '{0:03b}'.format(int(str(mm)[0]))
-    if(tens[2] == "1"):
-        sense.set_pixel(3,7,blue)
-    if(tens[1] == "1"):
-        sense.set_pixel(3,6,blue)
-    if(tens[0] == "1"):
-        sense.set_pixel(3,5,blue)
+    for y in range(3):
+        if(tens[y] == "1"):
+            sense.set_pixel(3, 5+y, blue)
 
     #mm 1s place
     ones = '{0:04b}'.format(int(str(mm)[1]))
-    if(ones[3] == "1"):
-        sense.set_pixel(4,7,blue)
-    if(ones[2] == "1"):
-        sense.set_pixel(4,6,blue)
-    if(ones[1] == "1"):
-        sense.set_pixel(4,5,blue)
-    if(ones[0] == "1"):
-        sense.set_pixel(4,4,blue)
+    for i in range(4):
+        if(ones[i] == "1"):
+            sense.set_pixel(4,4+i,blue)
 
 def setSeconds(ss=0):
     #ss 10s place
     tens = '{0:03b}'.format(int(str(ss)[0]))
-    if(tens[2] == "1"):
-        sense.set_pixel(6,7,blue)
-    if(tens[1] == "1"):
-        sense.set_pixel(6,6,blue)
-    if(tens[0] == "1"):
-        sense.set_pixel(6,5,blue)
+    for y in range(3):
+        if(tens[y] == "1"):
+            sense.set_pixel(6, 5+y, blue)
 
      #ss 1s place
     ones = '{0:04b}'.format(int(str(ss)[1]))
-    if(ones[3] == "1"):
-        sense.set_pixel(7,7,blue)
-    if(ones[2] == "1"):
-        sense.set_pixel(7,6,blue)
-    if(ones[1] == "1"):
-        sense.set_pixel(7,5,blue)
-    if(ones[0] == "1"):
-        sense.set_pixel(7,4,blue)
+    for i in range(4):
+        if(ones[i] == "1"):
+            sense.set_pixel(7,4+i,blue)
 
 white = (255,255,255)
 while True:
