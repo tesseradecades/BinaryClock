fcf5e4d732a90c67109a5a38ae9be094824e4fb3
tesseradecades <nwe8978@rit.edu>
2017-04-26 13:03:39 -0400

    abstracted out get ones and tens methods

--- displayTime.py
+++ displayTime.py
@@ -12,63 +12,78 @@ def getTime():
     return str(datetime.now()).split(" ")[1].split(".")[0].split(":")
 
 """
+Returns the string representation of the ones digit of a number in binary
+    ex) getOnes(9 -> 1001)
+
+num - the number to convert
+"""
+def getOnes(num):
+    strNum = str(num)
+    return '{0:04b}'.format(int(strNum[len(strNum)-1]))
+
+"""
+Returns the string representation of the tens digit of a number <=79 in binary
+    ex) getTens(79 -> 111)
+
+num - the number to convert
+"""
+def getTens(num):
+    strNum = str(num)
+    if(int(num) < 10):
+        strNum = "0"
+    return '{0:03b}'.format(int(strNum[len(strNum)-2]))
+
+"""
 Sets the pixels for the hour
 
-hh - integer representation of the hour to be set
+hh - string representation of the hour to be set
 
 """
 def setHours(hh=0):
     #hh 10s place
-    tens = '{0:03b}'.format(int(str(hh)[0]))
-    setTens(tens,0)
+    setTens(hh,0)
 
     #hh 1s place
-    ones = '{0:04b}'.format(int(str(hh)[1]))
-    setOnes(ones, 1)
+    setOnes(hh, 1)
 
-(":")
 
 """
 Sets the pixels for the minute
 
-mm - integer representation of the minute to be set
+mm - string representation of the minute to be set
 
 """
 def setMinutes(mm=0):
     #mm 10s place
-    tens = '{0:03b}'.format(int(str(mm)[0]))
-    setTens(tens, 3)
+    setTens(mm, 3)
 
     #mm 1s place
-    ones = '{0:04b}'.format(int(str(mm)[1]))
-    setOnes(ones, 4)
+    setOnes(mm, 4)
 
 """
 Sets the pixels for the second
 
-ss - integer representation of the second to be set
+ss - string representation of the second to be set
 
 """
 def setSeconds(ss=0):
     #ss 10s place
-    tens = '{0:03b}'.format(int(str(ss)[0]))
-    setTens(tens,6)
+    setTens(ss,6)
     
     #ss 1s place
-    ones = '{0:04b}'.format(int(str(ss)[1]))
-    setOnes(ones, 7)
+    setOnes(ss, 7)
 
 
 """
 Sets the appropriate pixels for the ones column of the number entered
 
-oneString - A binary representation of the number belonging in the ones column
-    ex)"1001" = 8
+num - string representation of the number to be set
 
 pixelColumn - the column value of the pixels used to represent the ones place
     seconds -> 7, minutes -> 4, hours -> 1
 """
-def setOnes(oneString="",pixelColumn=0):
+def setOnes(num="",pixelColumn=0):
+    oneString = getOnes(num)
     for i in range(4):
         if(oneString[i] == "1"):
             sense.set_pixel(pixelColumn, 4+i, blue)
@@ -76,13 +91,13 @@ def setOnes(oneString="",pixelColumn=0):
 """
 Sets the appropriate pixels for the ones column of the number entered
 
-oneString - A binary representation of the number belonging in the ones column
-    ex)"1001" = 8
+num - string representation of the number to be set
 
 pixelColumn - the column value of the pixels used to represent the ones place
     seconds -> 6, minutes -> 3, hours -> 0
 """
-def setTens(tenString="", pixelColumn=0):
+def setTens(num="", pixelColumn=0):
+    tenString = getTens(num)
     for i in range(3):
         if(tenString[i] == "1"):
             sense.set_pixel(pixelColumn,5+i, blue)
