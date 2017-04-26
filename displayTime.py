048a7f02e43ea31f86dc7d0c4156d1ca556e9c12
tesseradecades <nwe8978@rit.edu>
2017-04-26 04:42:42 +0000

    displays test time correctly. Next, parse current time for display

--- displayTime.py
+++ displayTime.py
@@ -8,36 +8,68 @@ sense = SenseHat()
 
 blue = (0, 0, 255)
 
-#hh 10s place
-sense.set_pixel(0,7,blue)
-sense.set_pixel(0,6,blue)
+def setHours(hh=0):
+    #hh 10s place
+    tens = '{0:02b}'.format(int(str(hh)[0]))
+    if(tens[1] == "1"):
+        sense.set_pixel(0,7,blue)
+    if(tens[0] == "1"):
+        sense.set_pixel(0,6,blue)
 
-#hh 1s place
-sense.set_pixel(1,7,blue)
-sense.set_pixel(1,6,blue)
-sense.set_pixel(1,5,blue)
-sense.set_pixel(1,4,blue)
+    #hh 1s place
+    ones = '{0:04b}'.format(int(str(hh)[1]))
+    if(ones[3] == "1"):
+        sense.set_pixel(1,7,blue)
+    if(ones[2] == "1"):
+        sense.set_pixel(1,6,blue)
+    if(ones[1] == "1"):
+        sense.set_pixel(1,5,blue)
+    if(ones[0] == "1"):
+        sense.set_pixel(1,4,blue)
 
-#mm 10s place
-sense.set_pixel(3,7,blue)
-sense.set_pixel(3,6,blue)
-sense.set_pixel(3,5,blue)
+def setMinutes(mm=0):
+    #mm 10s place
+    tens = '{0:03b}'.format(int(str(mm)[0]))
+    if(tens[2] == "1"):
+        sense.set_pixel(3,7,blue)
+    if(tens[1] == "1"):
+        sense.set_pixel(3,6,blue)
+    if(tens[0] == "1"):
+        sense.set_pixel(3,5,blue)
 
-#mm 1s place
-sense.set_pixel(4,7,blue)
-sense.set_pixel(4,6,blue)
-sense.set_pixel(4,5,blue)
-sense.set_pixel(4,4,blue)
+    #mm 1s place
+    ones = '{0:04b}'.format(int(str(mm)[1]))
+    if(ones[3] == "1"):
+        sense.set_pixel(4,7,blue)
+    if(ones[2] == "1"):
+        sense.set_pixel(4,6,blue)
+    if(ones[1] == "1"):
+        sense.set_pixel(4,5,blue)
+    if(ones[0] == "1"):
+        sense.set_pixel(4,4,blue)
 
-#ss 10s place
-sense.set_pixel(6,7,blue)
-sense.set_pixel(6,6,blue)
-sense.set_pixel(6,5,blue)
+def setSeconds(ss=0):
+    #ss 10s place
+    tens = '{0:03b}'.format(int(str(ss)[0]))
+    if(tens[2] == "1"):
+        sense.set_pixel(6,7,blue)
+    if(tens[1] == "1"):
+        sense.set_pixel(6,6,blue)
+    if(tens[0] == "1"):
+        sense.set_pixel(6,5,blue)
 
-#ss 1s place
-sense.set_pixel(7,7,blue)
-sense.set_pixel(7,6,blue)
-sense.set_pixel(7,5,blue)
-sense.set_pixel(7,4,blue)
+     #ss 1s place
+    ones = '{0:04b}'.format(int(str(ss)[1]))
+    if(ones[3] == "1"):
+        sense.set_pixel(7,7,blue)
+    if(ones[2] == "1"):
+        sense.set_pixel(7,6,blue)
+    if(ones[1] == "1"):
+        sense.set_pixel(7,5,blue)
+    if(ones[0] == "1"):
+        sense.set_pixel(7,4,blue)
 
+setHours(10)
+setMinutes(37)
+setSeconds(49)
 #sense.clear()
